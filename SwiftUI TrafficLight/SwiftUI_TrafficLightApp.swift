a1aa0e83fdbfcf5e18290765bea9ae4eb5027e60
Бруйко Евгения <bruyko.e@gmail.com>
2020-12-10 20:02:41 +0300

    Initial Commit

---
+++ SwiftUI TrafficLight/SwiftUI_TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TrafficLightApp.swift
+//  SwiftUI TrafficLight
+//
+//  Created by Brubrusha on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
